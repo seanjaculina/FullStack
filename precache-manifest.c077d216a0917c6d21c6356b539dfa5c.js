558fa7054deb4ab1a46feed5324269bf8d25abd6
Tanner Barcelos <tannerbarcelos@Tanners-MBP.hsd1.ca.comcast.net>
2020-05-16 23:58:36 -0700

    Updates

---
+++ precache-manifest.c077d216a0917c6d21c6356b539dfa5c.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "e51f6f2e724698909475673d8fe3b015",
+    "url": "/index.html"
+  },
+  {
+    "revision": "a034d99d54533418dc13",
+    "url": "/static/css/main.94cf1abd.chunk.css"
+  },
+  {
+    "revision": "88db55f0f5b17a2983c9",
+    "url": "/static/js/2.064513b9.chunk.js"
+  },
+  {
+    "revision": "e88a3e95b5364d46e95b35ae8c0dc27d",
+    "url": "/static/js/2.064513b9.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "a034d99d54533418dc13",
+    "url": "/static/js/main.2832341d.chunk.js"
+  },
+  {
+    "revision": "1a0b79f8749ea73b6cc9",
+    "url": "/static/js/runtime-main.8a884784.js"
+  }
+]);
