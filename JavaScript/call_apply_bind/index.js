c18aa77fc262c971f7edbe7e73d19673a9529f21
Tanner Barcelos <tannerbarcelos@Tanners-MBP.hsd1.ca.comcast.net>
2020-05-24 18:22:18 -0700

    call,apply,bind

--- JavaScript/call_apply_bind/index.js
+++ JavaScript/call_apply_bind/index.js
@@ -2,6 +2,10 @@ let obj = {
   num: 2,
 };
 
+let obj2 = {
+  num: 5,
+};
+
 //has to explicetely be an es5- function
 // let addThis = function (a) {
 //   return this.num + a; //as we can see, this refers to nothing with num in this function.. num is a property of obj! So, to use this, we can use call()
@@ -17,7 +21,7 @@ let obj = {
 //APPLY() for appling functions to objects and taking x amount of values to apply the fucntion to on the prop itself
 
 //we can also apply N arguments to an object to call that function on: we pass the
-//collection or list as the second argument now, and it will use each value as the corresponding value in our function to add with
+//array as the second argument now, and it will use each value as the corresponding value in our function to add with
 
 let addThis = function (a, b, c) {
   return this.num + a + b + c;
@@ -27,3 +31,15 @@ let addThis = function (a, b, c) {
 
 const arr = [1, 2, 3];
 console.log (addThis.apply (obj, arr)); //output: 8
+console.log (addThis.apply (obj2, arr)); // 11 -> we can use apply to multiple objects with miultiple calls of course
+
+/************************************************************************* */
+
+//BIND(objtobindto)
+//bind will attach the function as a method of that object. it returns a function with the new object. it does not call the function!
+//it just lets us use it at another time. we can simply call that function when needed, and it will refer to 'this' context
+//of the class/object it resides in
+
+const bound = addThis.bind (obj); //addthis is now bound to the obj and we can call it passing the x amount of arguments add this requires
+//this is similar to the events in react! we need to bind thos emethods to 'this' in the class so it can access all the properties of that instance
+console.log (bound (1, 2, 3)); // this.num = 2 as we know in the object, we then bind addThis to the object. we can then call that bound (the returned function from bind) with the arguments and change the state of the oibjecr we bound to
