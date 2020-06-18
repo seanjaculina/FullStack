7ad7b186d663ce1e8fec0db639159174b95e08ba
Tanner Barcelos <tanner.manuel.barcelos@gmail.com>
2020-06-17 17:21:22 -0700

    comments

--- JavaScript/MovieApp/assets/scripts/app.js
+++ JavaScript/MovieApp/assets/scripts/app.js
@@ -1,23 +1,21 @@
-const addMovieModal = document.getElementById('add-modal');
-// const addMovieModal = document.querySelector('#add-modal');
-// const addMovieModal = document.body.children[1];
-const startAddMovieButton = document.querySelector('header button');
-// const startAddMovieButton = document.querySelector('header').lastElementChild;
-const backdrop = document.getElementById('backdrop');
-// const backdrop = document.body.firstElementChild;
-const cancelAddMovieButton = addMovieModal.querySelector('.btn--passive');
+const addMovieModal = document.getElementById ('add-modal');
+const startAddMovieButton = document.querySelector ('header button');
+const backdrop = document.getElementById ('backdrop');
+const cancelAddMovieButton = addMovieModal.querySelector ('.btn--passive');
 const confirmAddMovieButton = cancelAddMovieButton.nextElementSibling;
-const userInputs = addMovieModal.querySelectorAll('input');
-// const userInputs = addMovieModal.getElementsByTagName('input');
-const entryTextSection = document.getElementById('entry-text');
-const deleteMovieModal = document.getElementById('delete-modal');
+const userInputs = addMovieModal.querySelectorAll ('input');
+const entryTextSection = document.getElementById ('entry-text');
+const deleteMovieModal = document.getElementById ('delete-modal');
 
+//holds list of movies for local storage in the script: the dom also will need to be updated with node list to remove/add movies
 const movies = [];
 
 const toggleBackdrop = () => {
-  backdrop.classList.toggle('visible');
+  //use toggle to turn 'on' or 'off' styles on an element
+  backdrop.classList.toggle ('visible');
 };
 
+//the 'your personal movie database' title thing [shpwing and nont showing it depending on movie list length]
 const updateUI = () => {
   if (movies.length === 0) {
     entryTextSection.style.display = 'block';
@@ -26,9 +24,11 @@ const updateUI = () => {
   }
 };
 
+//removes the class visible from the modal div succh that the modal will dissappear till its shown again. this is how we remove styles
+//dynamically to make the page seem more interactive!
 const closeMovieDeletionModal = () => {
-  toggleBackdrop();
-  deleteMovieModal.classList.remove('visible');
+  toggleBackdrop ();
+  deleteMovieModal.classList.remove ('visible');
 };
 
 const deleteMovieHandler = movieId => {
@@ -39,38 +39,47 @@ const deleteMovieHandler = movieId => {
     }
     movieIndex++;
   }
-  movies.splice(movieIndex, 1);
-  const listRoot = document.getElementById('movie-list');
-  listRoot.children[movieIndex].remove();
-  // listRoot.removeChild(listRoot.children[movieIndex]);
-  closeMovieDeletionModal();
-  updateUI();
+
+  //remove the movie from the array
+  movies.splice (movieIndex, 1);
+
+  //grab the movie list container in the DOM and remove the child by the index we found from searching by ID
+  const listRoot = document.getElementById ('movie-list');
+  listRoot.children[movieIndex].remove ();
+
+  /**
+   * Notice how we remove from array then from children [list items of the ul]. removing from the array in our script only removes the reference to the movie in our local code here so the code works right
+   * but the DOM needs to physically remove the node so its no longer seen! So, we must use the found item and remove it from the children html collection (dom nodes of li's)
+   */
+
+  closeMovieDeletionModal ();
+  updateUI ();
 };
 
 const startDeleteMovieHandler = movieId => {
-  deleteMovieModal.classList.add('visible');
-  toggleBackdrop();
+  deleteMovieModal.classList.add ('visible');
+  toggleBackdrop ();
 
-  const cancelDeletionButton = deleteMovieModal.querySelector('.btn--passive');
-  let confirmDeletionButton = deleteMovieModal.querySelector('.btn--danger');
+  const cancelDeletionButton = deleteMovieModal.querySelector ('.btn--passive');
+  let confirmDeletionButton = deleteMovieModal.querySelector ('.btn--danger');
 
-  confirmDeletionButton.replaceWith(confirmDeletionButton.cloneNode(true));
+  confirmDeletionButton.replaceWith (confirmDeletionButton.cloneNode (true));
 
-  confirmDeletionButton = deleteMovieModal.querySelector('.btn--danger');
+  confirmDeletionButton = deleteMovieModal.querySelector ('.btn--danger');
 
-  // confirmDeletionButton.removeEventListener('click', deleteMovieHandler.bind(null, movieId)); // will not work :(
-    
-  cancelDeletionButton.removeEventListener('click', closeMovieDeletionModal);
+  // confirmDeletionButton.removeEventListener('click', deleteMovieHandler.bind(this, movieId)); // will not work :(
 
-  cancelDeletionButton.addEventListener('click', closeMovieDeletionModal);
-  confirmDeletionButton.addEventListener(
+  cancelDeletionButton.removeEventListener ('click', closeMovieDeletionModal);
+
+  cancelDeletionButton.addEventListener ('click', closeMovieDeletionModal);
+  confirmDeletionButton.addEventListener (
     'click',
-    deleteMovieHandler.bind(null, movieId)
+    deleteMovieHandler.bind (this, movieId)
   );
 };
 
 const renderNewMovieElement = (id, title, imageUrl, rating) => {
-  const newMovieElement = document.createElement('li');
+  const newMovieElement = document.createElement ('li');
   newMovieElement.className = 'movie-element';
   newMovieElement.innerHTML = `
     <div class="movie-element__image">
@@ -81,22 +90,25 @@ const renderNewMovieElement = (id, title, imageUrl, rating) => {
       <p>${rating}/5 stars</p>
     </div>
   `;
-  newMovieElement.addEventListener(
+  newMovieElement.addEventListener (
     'click',
-    startDeleteMovieHandler.bind(null, id)
+    startDeleteMovieHandler.bind (this, id) //we are using an event listener on the movie list item: we are not using an arrow method
+    //here [can of course but tutorial is teaching us point of bind] and so we need to bind the callbacck to 'this' element in the dom and pass it an id
+    //so we can use the id to search the movie to delete. Bind always is negated when using () => funcitons (as we know in our react issues [which is a javascript thing])
+    //this is just showing that in order to call functions on elements in the dom dynamically, we lose context to 'this' objects context thus, the function will not know what to
+    //do and on what, so, binding to 'this' like this is the best way to do this
   );
-  const listRoot = document.getElementById('movie-list');
-  listRoot.append(newMovieElement);
+  const listRoot = document.getElementById ('movie-list');
+  listRoot.append (newMovieElement);
 };
 
 const closeMovieModal = () => {
-  addMovieModal.classList.remove('visible');
+  addMovieModal.classList.remove ('visible');
 };
 
 const showMovieModal = () => {
-  // function() {}
-  addMovieModal.classList.add('visible');
-  toggleBackdrop();
+  addMovieModal.classList.add ('visible'); //add the visible class to the modal to show it (css will do al the nice animation, as we know)
+  toggleBackdrop ();
 };
 
 const clearMovieInput = () => {
@@ -106,9 +118,9 @@ const clearMovieInput = () => {
 };
 
 const cancelAddMovieHandler = () => {
-  closeMovieModal();
-  toggleBackdrop();
-  clearMovieInput();
+  closeMovieModal ();
+  toggleBackdrop ();
+  clearMovieInput ();
 };
 
 const addMovieHandler = () => {
@@ -117,44 +129,53 @@ const addMovieHandler = () => {
   const ratingValue = userInputs[2].value;
 
   if (
-    titleValue.trim() === '' ||
-    imageUrlValue.trim() === '' ||
-    ratingValue.trim() === '' ||
+    titleValue.trim () === '' ||
+    imageUrlValue.trim () === '' ||
+    ratingValue.trim () === '' ||
     +ratingValue < 1 ||
     +ratingValue > 5
   ) {
-    alert('Please enter valid values (rating between 1 and 5).');
+    alert ('Please enter valid values (rating between 1 and 5).');
     return;
   }
 
   const newMovie = {
-    id: Math.random().toString(),
+    id: Math.random ().toString (),
     title: titleValue,
     image: imageUrlValue,
-    rating: ratingValue
+    rating: ratingValue,
   };
 
-  movies.push(newMovie);
-  console.log(movies);
-  closeMovieModal();
-  toggleBackdrop();
-  clearMovieInput();
-  renderNewMovieElement(
+  movies.push (newMovie);
+  console.log (movies);
+  closeMovieModal ();
+  toggleBackdrop ();
+  clearMovieInput ();
+  renderNewMovieElement (
     newMovie.id,
     newMovie.title,
     newMovie.image,
     newMovie.rating
   );
-  updateUI();
+  updateUI ();
 };
 
 const backdropClickHandler = () => {
-  closeMovieModal();
-  closeMovieDeletionModal();
-  clearMovieInput();
+  closeMovieModal ();
+  closeMovieDeletionModal ();
+  clearMovieInput ();
 };
 
-startAddMovieButton.addEventListener('click', showMovieModal);
-backdrop.addEventListener('click', backdropClickHandler);
-cancelAddMovieButton.addEventListener('click', cancelAddMovieHandler);
-confirmAddMovieButton.addEventListener('click', addMovieHandler);
+startAddMovieButton.addEventListener ('click', showMovieModal);
+backdrop.addEventListener ('click', backdropClickHandler);
+cancelAddMovieButton.addEventListener ('click', cancelAddMovieHandler);
+confirmAddMovieButton.addEventListener ('click', addMovieHandler);
+
+/**
+ * Event listeners must bind the event method to the item it is called on so we keep within the context of 'this' object (As we have seen a common issue in listeners on the react dom
+ * objects too [which was said to us IT IS A JAVASCRIPT THING, HENCE DOING IT HERE]) and a way to avoid the binding issues, we can always simply use the anonymous ()=> method as often seen in 
+ * events and also seen when making the methods arrow functions in react. just rmemeber if we are using a callback, we do not want to invoke it in react, we just reference the arrow function for binding 
+ * but in vanillaJS like we do here, we simply bind to the function if its not anonymous, or use anonymous right within the bound event
+ * 
+ * the easier concept is making all event listeners anonymous to implicitely bind our events, but, we also should know about bind because bind is a big deal
+ */
