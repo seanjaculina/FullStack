f40895a2aa4594bb7aea1cef98f32be44be09bde
Tanner Barcelos <tanner.manuel.barcelos@gmail.com>
2020-07-10 14:45:40 -0700

    init

---
+++ DataStructures_Algorithms/problemSolvingPatterns/multiplePointerPattern/sumZero/index.js
@@ -0,0 +1,18 @@
+/**
+ * Problem statement:
+ * 
+ *  Write a function called sumZero which accepts a sorted array of intergers. The
+ *  function should find the first pair where the sum is 0. Return an array that
+ *  includes both values that sum to zero or undefined if a pair dne
+ * 
+ * 
+ *  examples:
+ *    
+ *      sumZero([-3,-2,-1,0,1,2,3])   // [-3,3]
+ *      sumZero([-2,0,1,3])           // undefined
+ *      sumZero([1,2,3])              // undefined
+ */
+
+const sumZero = (arr) => {
+
+}
