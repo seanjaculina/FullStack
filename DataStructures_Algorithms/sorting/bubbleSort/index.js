1cc6b5c5d09110914a32ec3fad37a5fd710790a8
Tanner Barcelos <tanner.manuel.barcelos@gmail.com>
2020-07-15 00:06:18 -0700

    1 more

--- DataStructures_Algorithms/sorting/bubbleSort/index.js
+++ DataStructures_Algorithms/sorting/bubbleSort/index.js
@@ -10,6 +10,7 @@ const bubbleSort = arr => {
   for (let i = arr.length; i > 0; i--) {
     noSwaps = true;
     for (let j = 0; j < i - 1; j++) {
+      // checking in ascending: flip sign to make the sort in descending
       if (arr[j] > arr[j + 1]) {
         // swap
         swap(arr, j, j + 1)
