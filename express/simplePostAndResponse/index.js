3b7d8a9c2db57627d2615b7e103393fb84d2165f
Tanner Barcelos <tanner.manuel.barcelos@gmail.com>
2020-07-20 12:42:24 -0700

    done

--- express/simplePostAndResponse/index.js
+++ express/simplePostAndResponse/index.js
@@ -24,8 +24,23 @@ app.get('/', (req, res) => {
 
 
 app.post('/submitData', (req, res) => {
+
+  // we can destructure the data too
+  const {
+    name,
+    comment
+  } = req.body;
+
+  // if either or no data is entered
+  if (!name || !comment) {
+    return res.json({
+      "msg": "Please enter data"
+    })
+  }
+
+  //IMPORTANT!
   // req.body will contian all fields from the specified form sending data to this route via the 'name' attribute on all the form fields
-  res.send(`Name: ${req.body.name}  Comment: ${req.body.comment}`)
+  res.send(`Name: ${name}  Comment: ${comment}`)
 
   // often we like to send json back as a response for certain things.. the key thing here is that we can get the form data via req.body.NAME_ATTRIBUTE_ON_PARTICULAR_FORM_FIELD
   // res.json({
