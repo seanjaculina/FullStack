9f6c5f7295dcfbfed1475792750a9c740b198541
Tanner Barcelos <tanner.manuel.barcelos@gmail.com>
2020-07-22 15:25:01 -0700

    ignore commit - switch to Djangoi

--- Node/FullStackProjectResourceForLearning-MongoNodeExpressTemplates/server.js
+++ Node/FullStackProjectResourceForLearning-MongoNodeExpressTemplates/server.js
@@ -3,23 +3,20 @@ const app = express(); // create the express app instance
 const expressLayouts = require('express-ejs-layouts'); // importing express layouts for ejs -
 
 
-// set ejs as our view engine -> no need for this if using react as a client side view
+// set ejs as our view engine and layout entry point and view directory
 app.set('view engine', 'ejs');
-app.set('views', __dirname + '/views'); // sets the views folder in our project to be the source for express to render views from
+app.set('views', __dirname + '/views');
 app.set('layout', 'layouts/layout');
-
 // middlewares
 app.use(expressLayouts);
 app.use(express.static('public')); // tell express what folder all our static content like html/css/img's etc. will be coming from
 
 
-// Test route
-app.get('/', (req, res) => {
-  res.send('<h1>Hello!</h1>')
-})
+// routes in the app : common routes are allowed in separate files and matched suing express router
+const indexRouter = require('./routes/index');
 
-
-
+// set up the route handlers
+app.use('/', indexRouter);
 
 
 // Listen -> Save port and listen
