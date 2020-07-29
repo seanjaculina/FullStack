14db2e5bade2f021d1139d90a2e5b0759abe714e
Tanner Barcelos <tanner.manuel.barcelos@gmail.com>
2020-07-28 23:34:52 -0700

    refactoring and studying basics of react

--- React/car-tiles/car-tiles/src/components/ImageCard.js
+++ React/car-tiles/car-tiles/src/components/ImageCard.js
@@ -1,7 +1,7 @@
 import React, { Component } from "react";
 
 //this component returns a single image as a car which receives
-//the image through map in the lsit component
+//the image through map in the list component
 
 class ImageCard extends Component {
   constructor(props) {
@@ -12,23 +12,18 @@ class ImageCard extends Component {
     this.imageRef = React.createRef();
 
     this.state = {
-      spans: 0
+      spans: 0,
     };
 
-    //bind our events ALWAYS
-    this.setSpans = this.setSpans.bind(this);
+    //bind our events ALWAYS unless using =>
   }
 
-  //when the component mounts, we know this mehtod is used to gather data once, and use it once it shows up, so,
-  //lets grab the images width and height with our ref (clientHeight and width)
-  //the only caveat is: the image loads longer than the component takes to show up in the dom, so by default, we will
-  //get 0 values, so, we had to handle this by adding an event('load) to the image and using a callback to span the image (used to just re-adjust the size to evenly distribute image cards)
-  //remember all events (any event) must be bound to 'this' i like doing the bindings in constructor but the other work around is making the event callback method an arrow method
+  // Runs immediately after render
   componentDidMount() {
     this.imageRef.current.addEventListener("load", this.setSpans);
   }
 
-  setSpans() {
+  setSpans = () => {
     //get image height
     const height = this.imageRef.current.clientHeight;
 
@@ -37,17 +32,13 @@ class ImageCard extends Component {
 
     //set the span number for this image for state and pass the state to the div as the span grid row end style inline
     this.setState({ spans });
-  }
+  };
 
   render() {
-    //destructure the images description and url link to that image so we
-    //can use those vars
+    //destructure the images description and url link to that image
     const { desciption, urls } = this.props.image;
     return (
-      /*lookup why we are doing styling for gridrowend on a grid card: this handles the size*/
       <div style={{ gridRowEnd: `span ${this.state.spans}` }}>
-        {/*attach the ref to THIS image (we have a ton of them on search submit, that way each image
-        component will have a ref to all its client data*/}
         <img ref={this.imageRef} alt={desciption} src={urls.regular} />
       </div>
     );
--- React/car-tiles/car-tiles/src/components/ImageList.js
+++ React/car-tiles/car-tiles/src/components/ImageList.js
@@ -6,18 +6,20 @@ import "../static/ImageList.css";
 //component imports
 import ImageCard from "./ImageCard";
 
-const ImageList = props => {
-  console.log(props.images);
-
-  //map all the images returning an image card of the queried image (pass the image as prop in which we can destructure in card)
-  const images = props.images.map(img => {
-    return <ImageCard key={img.id} image={img} />;
-  });
+// Functional component - pulls out the image prop
+const ImageList = ({ images }) => {
+  // Helper function to render images and modularize our code a bit - conditonally renders images if there happens to be images in the props
+  const renderImages = () => {
+    return images
+      ? images.map((img) => {
+          return <ImageCard key={img.id} image={img} />;
+        })
+      : null;
+  };
 
   return (
     <div>
-      {/*we are going to use css grid for the image cards*/}
-      <div className="image-list">{images}</div>
+      <div className="image-list">{renderImages()}</div>
     </div>
   );
 };
