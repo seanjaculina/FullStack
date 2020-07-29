d6b932bea9ab2a812250a186813456da0960abf0
Tanner Barcelos <tanner.manuel.barcelos@gmail.com>
2020-07-28 23:38:05 -0700

    Add

--- React/car-tiles/car-tiles/src/api/unsplash.js
+++ React/car-tiles/car-tiles/src/api/unsplash.js
@@ -1,12 +1,10 @@
 import axios from "axios";
-import { key, url } from "../config"; //config file holding our env_vars {only pull out key and the url to base off of}
+import { key, url } from "../config";
 
-//create a cusom request: takes the url to search and the headers as an object:
-//i imported my env vars which is why i am using string interpolation
+// Create an axios instance to be used in any file to submit axios calls
 export default axios.create({
-  //create takes in the base url and any headers to do the request: in the get method in client, we pass the sub route (search/images only)
   baseURL: `${url}`,
   headers: {
-    Authorization: `Client-ID ${key}` //string interpolation since we are using a variable to store this
-  }
+    Authorization: `Client-ID ${key}`,
+  },
 });
