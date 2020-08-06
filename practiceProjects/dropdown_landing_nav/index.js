fa0996559d2dda1f08b9d5e532156749cbcda51d
Tanner Barcelos <tanner.manuel.barcelos@gmail.com>
2020-08-06 15:03:09 -0700

    added dynamic styling to the hovers

---
+++ practiceProjects/dropdown_landing_nav/index.js
@@ -0,0 +1,24 @@
+// grab the about and contact list items
+const about = document.querySelector("#about");
+const contact = document.querySelector("#contact");
+
+// Grab the dropdown containers
+const aboutDrop = document.querySelector(".about-dropdown");
+const contactDrop = document.querySelector(".contact-dropdown");
+
+// hover listener on about hover
+about.addEventListener("mouseover", () => {
+  aboutDrop.classList.add("show");
+  // array methods for adding classes/seeing if they exist (add,remove, contains)
+});
+about.addEventListener("mouseleave", () => {
+  aboutDrop.classList.remove("show");
+});
+
+// hover listener on contact hover
+contact.addEventListener("mouseover", () => {
+  contactDrop.classList.add("show");
+});
+contact.addEventListener("mouseleave", () => {
+  contactDrop.classList.remove("show");
+});
