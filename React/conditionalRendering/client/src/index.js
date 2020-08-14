fd537bc319fa418ece4d96298930eb18ec448571
Tanner Barcelos <tanner.manuel.barcelos@gmail.com>
2020-08-14 13:17:44 -0700

    finished with basics of conditional rendering

--- React/conditionalRendering/client/src/index.js
+++ React/conditionalRendering/client/src/index.js
@@ -1,13 +1,14 @@
-import React from "react";
+import React, { Fragment } from "react";
 import ReactDOM from "react-dom";
 import "./index.css";
-import App from "./App";
+import App from "./components/App";
 import * as serviceWorker from "./serviceWorker";
 
 ReactDOM.render(
-  <>
+  // or simply <> </>
+  <Fragment>
     <App />
-  </>,
+  </Fragment>,
   document.querySelector("#root")
 );
 
