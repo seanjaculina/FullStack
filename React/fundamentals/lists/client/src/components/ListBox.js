a17718e9ab6319f582a730c6163c31dc5be1e112
Tanner Barcelos <tanner.manuel.barcelos@gmail.com>
2020-08-16 18:41:39 -0700

    refactor of lists

--- React/fundamentals/lists/client/src/components/ListBox.js
+++ React/fundamentals/lists/client/src/components/ListBox.js
@@ -9,15 +9,12 @@ import ListItem from "./ListItem";
 // Data import
 import { users } from "../data";
 
-// helper function to clean up the main component rendering - I like to separate logic from rendering pure JSX in my code
-const renderUserItemCard = (name, age, _id) => {
-  return <ListItem name={name} age={age} id={_id} key={_id} />;
-};
-
 const ListBox = () => {
   return (
     <div className="container">
-      {users.map(({ name, age, _id }) => renderUserItemCard(name, age, _id))}
+      {users.map(({ name, age, _id }) => (
+        <ListItem name={name} age={age} id={_id} key={_id} />
+      ))}
     </div>
   );
 };
