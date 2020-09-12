24a0f95de1c39ff092b584cfa1b0f893aa8134db
Tanner Barcelos <tanner.manuel.barcelos@gmail.com>
2020-09-12 12:51:40 -0700

    done for now

--- React/basic_list/list_app/src/Components/Activities.js
+++ React/basic_list/list_app/src/Components/Activities.js
@@ -1,4 +1,4 @@
-import React from "react";
+import React, { useState } from "react";
 
 // Data
 import activities from "../activities";
@@ -7,9 +7,23 @@ import activities from "../activities";
 import ActivityItem from "./ActivityItem";
 
 function Activities() {
+  const [items, setItems] = useState(activities);
+  const [selected, setSelected] = useState(0);
+
+  function handleClick(e, index) {
+    console.log(items);
+    setSelected(index);
+    setItems(activities.filter((item, indexOf) => indexOf !== index));
+  }
   function renderList() {
     return activities.map((item, index) => (
-      <ActivityItem name={item.name} _id={item._id} desc={item.desc} />
+      <ActivityItem
+        name={item.name}
+        _id={item._id}
+        desc={item.desc}
+        index={index}
+        handleClick={handleClick}
+      />
     ));
   }
 
--- React/basic_list/list_app/src/Components/ActivityItem.js
+++ React/basic_list/list_app/src/Components/ActivityItem.js
@@ -1,13 +1,28 @@
-import React from "react";
+import React, { useState } from "react";
 
-function Activity_Item({ name, _id, desc }) {
+function Activity_Item({ name, _id, desc, index, handleClick }) {
   return (
-    <div className="item" key={_id}>
+    <div
+      className="item"
+      key={_id}
+      style={{
+        marginTop: "3rem",
+        backgroundColor: "lightgray",
+        padding: "1rem",
+        borderRadius: "5px",
+      }}
+    >
       <div className="content">
         <h4 className="header">{name}</h4>
         <div className="description">{desc}</div>
       </div>
-      <button className="ui red button">Remove</button>
+      <button
+        className="ui red button"
+        style={{ marginTop: "10px" }}
+        onClick={(e) => handleClick(e, index)}
+      >
+        x
+      </button>
     </div>
   );
 }
