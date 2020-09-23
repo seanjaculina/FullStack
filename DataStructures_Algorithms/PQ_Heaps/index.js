54668fe0565cb3d09e3c08cef968554f0860e56a
Tanner Barcelos <tanner.manuel.barcelos@gmail.com>
2020-09-22 19:20:36 -0700

    continued work

--- DataStructures_Algorithms/PQ_Heaps/index.js
+++ DataStructures_Algorithms/PQ_Heaps/index.js
@@ -6,6 +6,43 @@ class Node {
   }
 }
 
+class Heap {
+  constructor() {
+    this.heapElements = [];
+  }
+
+  // inserting into a heap is very particular - we need to make sure it has parents that are greater than or less than this element depending if it is a min/max heap
+  insert(value) {
+    this.heapElements.push(value);
+
+    let currIndex = this.heapElements.length - 1; // get the last element (which is element we just inserted)
+    let parentOf = Math.floor((currIndex + 1) / 2) - 1; // find its parent
+
+    // while
+    while (
+      parentOf !== 0 &&
+      this.heapElements[currIndex] > this.heapElements[parentOf] // > since we are making a max heap
+    ) {
+      const parent = this.heapElements[parentOf];
+      this.heapElements[parentOf] = value;
+      this.heapElements[currIndex] = parent;
+      currIndex = parentOf;
+      parentOf = Math.floor((currIndex + 1) / 2) - 1;
+    }
+  }
+}
+
+const heap = new Heap();
+
+heap.insert(85);
+heap.insert(250);
+heap.insert(101);
+heap.insert(197);
+heap.insert(12);
+heap.insert(15);
+heap.insert(40);
+
+console.log(heap);
 /**
  * insert : O(N) worst-average / O(1) best
  * process : O(1)
@@ -63,4 +100,10 @@ console.log(pq);
  *
  * min/max heaps have many use cases for prioritizing order of small to big or big to small
  * data (used a lot in interviews) - we implement heaps using a priority queue as an array! super cool
+ *
+ *
+ * const heap = [250, 197, 85, 101, 12, 40, 15]
+ *
+ * get parent of 15 in the head --> Math.floor((heap[heap.length-1] + 1) / 2) - 1
+ *
  */
