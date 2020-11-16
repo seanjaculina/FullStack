7f95b0bfdbc207ee2e905b1ec7d712f6f0324c4d
Tanner Barcelos <tanner.manuel.barcelos@gmail.com>
2020-11-16 12:23:42 -0800

    about

--- react_portfolio/src/components/About.js
+++ react_portfolio/src/components/About.js
@@ -1,9 +1,37 @@
-import React from 'react'
+import React from 'react';
+import TechnologyItem from './TechnologyItem';
+
+import Me from '../images/me.jpg';
 
 const About = () => {
+
+  const iconArray = [
+    {"className":"fab fa-js" ,"technology":"js"},
+    {"className":"fab fa-react" ,"technology":"react"},
+    {"className":"fab fa-node" ,"technology":"node"},
+    {"className":"fab fa-html5" ,"technology":"html5"},
+    {"className":"fab fa-css3-alt" ,"technology":"css3"},
+    {"className":"fab fa-java" ,"technology":"java"},
+    {"className":"fab fa-python" ,"technology":"python"},
+    {"className":"fas fa-project-diagram" ,"technology":"data structures and algorithms"}
+  ];
+
   return (
     <div className="container">
-      <h1 className="section__heading">About me</h1>
+      <div className="about">
+        <h1 className="section__heading">About me</h1>
+        <div className="section__content">
+          <img alt="profile" src={Me}/>
+          <p>My name is Tanner and I am a rising senior at CSU East Bay in Hayward graduating this coming May.
+          I have an interest in full stack development and am interested in Frontend/Fullstack engineering roles. If 
+          See some of my technical skills below!</p>
+          <div className="technologies_container">
+            {
+              iconArray.map((icon,i) => <TechnologyItem key={i} icon={icon}/>)
+            }
+          </div>
+        </div>
+      </div>
     </div>
   )
 }
---
+++ react_portfolio/src/components/TechnologyItem.js
@@ -0,0 +1,24 @@
+import React from 'react'
+
+const TechnologyItem = ({icon}) => {
+
+  const iconColorMap = {
+    "js": "rgb(247,166,18)",
+    "react": "rgb(97,218,251)",
+    "node": "rgb(119,181,92)",
+    "html5": "rgb(228,77,38)",
+    "css3": "rgb(40,98,233)",
+    "java": "rgb(249,151,26)",
+    "python": "rgb(62,128,158)",
+    "data structures and algorithms": "#52057b"
+  }
+
+
+  return (
+    <>
+      <i className={icon.className} style={{color:iconColorMap[icon.technology]}} data-technology={icon.technology}></i>
+    </>
+  )
+}
+
+export default TechnologyItem;
