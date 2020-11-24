c6148c00a56a404ea3f4a0bdde93480dfe15ba3e
Tanner Barcelos <tanner.manuel.barcelos@gmail.com>
2020-11-23 22:05:00 -0800

    esmodule conversion, dotenv and more cleanup

--- MERN/projects/proshop/backend/data/products.js
+++ MERN/projects/proshop/backend/data/products.js
@@ -79,4 +79,4 @@ const products = [
   },
 ];
 
-module.exports = products;
+export default products; // using esmodules in Node! So this works
--- MERN/projects/proshop/backend/server.js
+++ MERN/projects/proshop/backend/server.js
@@ -1,7 +1,16 @@
-const express = require('express');
+import express from 'express'; // to use esmodules in node, we need to add a "type": "module" property to the root package.json
+import dotenv from 'dotenv'; // use .env
 const app = express();
 
-const products = require('./data/products');
+// config .env
+dotenv.config();
+
+import products from './data/products.js';
+/**
+ * Unlike in react or other client side esmodules, when we import
+ * directories or files from our local development environment (not npm modules like express, etc.)
+ * we NEED the .js or other file extension! See line 5 vs line 1
+ */
 
 app.get('/', (req, res) => {
   res.send('API running');
@@ -13,9 +22,11 @@ app.get('/api/products', (req, res) => {
 
 app.get('/api/products/:id', (req, res) => {
   const { id } = req.params; // pull out just the id param
-  const product = products.find((p) => p._id === id);
+  const product = find((p) => p._id === id);
   res.json(product);
 });
 
 const PORT = process.env.PORT || 5000;
-app.listen(PORT, () => console.log(`Server started on ${PORT}`));
+app.listen(PORT, () =>
+  console.log(`Server started on ${PORT} in ${process.env.NODE_ENV} mode`)
+);
