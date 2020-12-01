dfd1391f1690d52c94f2ed6f246de7d09e1a439a
Tanner Barcelos <tanner.manuel.barcelos@gmail.com>
2020-12-01 12:32:45 -0800

    more changes

---
+++ React/jsonplaceholder/src/index.js
@@ -0,0 +1,10 @@
+import React from 'react';
+import ReactDOM from 'react-dom';
+import './index.css';
+
+// Bootstrap stuff
+import 'bootstrap/dist/css/bootstrap.css';
+
+import App from './Components/App';
+
+ReactDOM.render(<App />, document.querySelector('#root'));
