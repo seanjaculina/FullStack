9cc2418c4cce552ee3579fa975a851fa1aeb5150
Tanner Barcelos <tanner.manuel.barcelos@gmail.com>
2020-12-01 17:10:07 -0800

    post senders met

--- React/jsonplaceholder/src/Components/Todos.js
+++ React/jsonplaceholder/src/Components/Todos.js
@@ -1,8 +1,23 @@
-import React from 'react';
+import React, { useState, useEffect } from 'react';
 import { Link } from 'react-router-dom';
 
 const Todos = ({ match }) => {
   console.log(match);
+  const [todoData, setTodos] = useState(null);
+
+  useEffect(() => {
+    (async () => {
+      // send request to the endpoint for todos and pass the userId into the query string to get that users data by the ID we passed
+      // if we were using express, we would get the data sent from here and then pull it out with req.params
+      const res = await fetch(
+        `https://jsonplaceholder.typicode.com/todos?userId=${match.params.id}`,
+      );
+      const data = await res.json();
+      setTodos(data);
+      console.log(data);
+    })();
+  }, [match.params.id]);
+
   return (
     <div className="container" style={{ margin: '1rem 1rem' }}>
       <h1>Todos</h1>
