20dd84a292f679a9bfd212f5ff5bbe900bc929a3
Tanner Barcelos <tanner.manuel.barcelos@gmail.com>
2020-12-11 15:00:37 -0800

    CSS responsive updates

--- React/tutorials/basicreactstate/dist/basicreactstate.e31bb0bc.js
+++ React/tutorials/basicreactstate/dist/basicreactstate.e31bb0bc.js
@@ -33459,7 +33459,7 @@ var parent = module.bundle.parent;
 if ((!parent || !parent.isParcelRequire) && typeof WebSocket !== 'undefined') {
   var hostname = "" || location.hostname;
   var protocol = location.protocol === 'https:' ? 'wss' : 'ws';
-  var ws = new WebSocket(protocol + '://' + hostname + ':' + "55652" + '/');
+  var ws = new WebSocket(protocol + '://' + hostname + ':' + "56902" + '/');
 
   ws.onmessage = function (event) {
     checkedAssets = {};
