9d452fc1cf62842ceda71e36918ac6fd4a81c774
Tanner Barcelos <tanner.manuel.barcelos@gmail.com>
2020-12-12 14:51:52 -0800

    more installs for client

--- Node/websockets/index.js
+++ Node/websockets/index.js
@@ -6,13 +6,7 @@ const app = express();
 const PORT = process.env.PORT || 5000;
 const server = app.listen(PORT, console.log(`Server started on port ${PORT}`));
 
-const io = socketIO(server);
-
 // Default router
 app.get('/', (req, res) => {
   res.send('<h1>Hello, world</h1>');
-});
-
-io.on('connection', (socket) => {
-  console.log(socket);
 });
