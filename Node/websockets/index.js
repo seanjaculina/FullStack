a8c88b51886246a00dc4705add7802aa0b24ba9f
Tanner Barcelos <tanner.manuel.barcelos@gmail.com>
2020-12-11 16:22:35 -0800

    init clean

--- Node/websockets/index.js
+++ Node/websockets/index.js
@@ -1,4 +1,5 @@
 import express from 'express';
+import colors from 'colors';
 
 // App initialization
 const app = express();
@@ -6,9 +7,11 @@ const app = express();
 // Middlewares
 
 // Init a port number
-const PORT = process.env.PORT || 3000;
+const PORT = process.env.PORT || 5000;
 
 // Create a server
-const server = app.listen(PORT, () =>
-  console.log(`Server started on port ${PORT}`),
+app.listen(PORT, () =>
+  console.log(
+    `Server started on ${PORT}`.magenta, // colors from npm
+  ),
 );
