74dde552c851fe0d7db775d94125901fc7a4fc73
Tanner Barcelos <tanner.manuel.barcelos@gmail.com>
2020-12-11 18:04:21 -0800

    enough

--- Node/websockets/index.js
+++ Node/websockets/index.js
@@ -1,26 +1,18 @@
 const express = require('express');
-const socket = require('socket.io');
-const http = require('http');
-const colors = require('colors');
+const socketIO = require('socket.io');
 
-// Init application
 const app = express();
 
+const PORT = process.env.PORT || 5000;
+const server = app.listen(PORT, console.log(`Server started on port ${PORT}`));
+
+const io = socketIO(server);
+
+// Default router
 app.get('/', (req, res) => {
-  res.json({ msg: 'Opened connection.' });
+  res.send('<h1>Hello, world</h1>');
 });
 
-// Create a server
-const server = http.createServer(app);
-
-// Express server startup
-const PORT = process.env.PORT || 5000;
-app.listen(PORT, () => console.log(`Sever listening on port ${PORT}`.magenta));
-
-// Instantiate a new web socket
-const io = socket(server);
-
-// Listen for a socket connection
 io.on('connection', (socket) => {
-  console.log(`Socket connection opened: ${socket.id}`);
+  console.log(socket);
 });
