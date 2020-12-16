cfe48fa90fe88576d4cbc32c3e317667e4458e1c
Tanner Barcelos <tanner.manuel.barcelos@gmail.com>
2020-12-15 23:33:28 -0800

    ig

---
+++ MERN/projects/proshop/frontend/src/components/screens/RegisterScreen.js
@@ -0,0 +1,117 @@
+import React, { useState, useEffect } from 'react';
+import { Link } from 'react-router-dom';
+import { Form, Button, Row, Col } from 'react-bootstrap';
+import { useDispatch, useSelector } from 'react-redux';
+
+import Message from '../Message';
+import Loader from '../Loader';
+import FormContainer from '../FormContainer';
+
+import { register } from '../../actions/userActions';
+
+const RegisterScreen = ({ location, history }) => {
+  const [name, setName] = useState('');
+  const [email, setEmail] = useState('');
+  const [password, setPassword] = useState('');
+  const [confirmPassword, setConfirmPassword] = useState('');
+  const [message, setMessage] = useState(null);
+
+  const dispatch = useDispatch();
+
+  // get the userLogin state (this specifically points to the userlogin reducer so we can fire actions off into this)
+  const userRegister = useSelector((state) => state.userRegister);
+  const { loading, error, userInfo } = userRegister;
+  const redirect = location.search ? location.search.split('=')[1] : '/';
+
+  // If we are already logged in, redirect
+  useEffect(() => {
+    if (userInfo) {
+      history.push(redirect);
+    }
+  }, [history, redirect, userInfo]);
+
+  const submitHandler = (e) => {
+    e.preventDefault();
+    if (password !== confirmPassword) {
+      setMessage('Passwords do not match');
+    } else {
+      dispatch(register(name, email, password)); // dispatch the login action to the store
+    }
+  };
+
+  const onHandleChange = (e) => {
+    // determine if this was the email or password field being updated
+    switch (e.target.type) {
+      case 'email':
+        setEmail(e.target.value);
+        break;
+      case 'password':
+        setPassword(e.target.value);
+        break;
+      case 'text':
+        setName(e.target.value);
+        break;
+      default:
+        break;
+    }
+  };
+
+  return (
+    <FormContainer>
+      <h1>Sign Up</h1>
+      {message && <Message variant="danger">{message}</Message>}
+      {error && <Message variant="danger">{error}</Message>}
+      {loading && <Loader />}
+      <Form onSubmit={submitHandler}>
+        <Form.Group controlId="name">
+          <Form.Label>Name</Form.Label>
+          <Form.Control
+            type="text"
+            placeholder="Enter name"
+            value={name}
+            onChange={onHandleChange}
+          ></Form.Control>
+        </Form.Group>
+        <Form.Group controlId="email">
+          <Form.Label>Email Address</Form.Label>
+          <Form.Control
+            type="email"
+            placeholder="Enter email"
+            value={email}
+            onChange={onHandleChange}
+          ></Form.Control>
+        </Form.Group>
+        <Form.Group controlId="password">
+          <Form.Label>Password</Form.Label>
+          <Form.Control
+            type="password"
+            placeholder="Enter password"
+            value={password}
+            onChange={onHandleChange}
+          ></Form.Control>
+        </Form.Group>
+        <Form.Group controlId="confirmPassword">
+          <Form.Label>Password</Form.Label>
+          <Form.Control
+            type="password"
+            placeholder="Confirm password"
+            value={confirmPassword}
+            onChange={(e) => setConfirmPassword(e.target.value)}
+          ></Form.Control>
+        </Form.Group>
+        <Button type="submit" variant="primary">
+          Register
+        </Button>
+      </Form>
+      <Row className="py-3">
+        <Col>
+          <Link to={redirect ? `/login?redirect=${redirect}` : '/login'}>
+            Login
+          </Link>
+        </Col>
+      </Row>
+    </FormContainer>
+  );
+};
+
+export default RegisterScreen;
