2b3437ff513d1dfe7a33fe1a9393bab8d9da34de
Tanner Barcelos <tanner.manuel.barcelos@gmail.com>
2020-12-16 19:56:44 -0800

    add

--- react_portfolio/src/components/RecommendationItem.js
+++ react_portfolio/src/components/RecommendationItem.js
@@ -7,10 +7,12 @@ const RecommendationItem = ({ data }) => {
         <Card.Title>{data.name}</Card.Title>
         <Card.Subtitle className="mb-2 text-muted">
           {data.position}
-          <br />
-          <small>{data.association}</small>
+          <hr />
+          <small style={{ display: 'block', padding: '0.3rem 0' }}>
+            {data.association}
+          </small>
         </Card.Subtitle>
-        <Card.Text>{data.recommendation}</Card.Text>
+        <Card.Text id="text_of">{data.recommendation}</Card.Text>
         <Card.Link
           target="_blank"
           href={data.linkedin}
