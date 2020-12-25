cfa2ec58e4373a56c906d3606026a07d522cce46
Tanner Barcelos <tanner.manuel.barcelos@gmail.com>
2020-12-25 01:12:12 -0800

    ready for Heroku

--- MERN/Traversy_Course/server.js
+++ MERN/Traversy_Course/server.js
@@ -1,4 +1,5 @@
 const express = require('express');
+const path = require('path');
 require('colors');
 const dbConnect = require('./db');
 require('dotenv').config();
@@ -11,6 +12,14 @@ app.use(express.json()); // to parse body data in requests
 
 // routes use/imports
 app.use('/api/tasks', require('./routes/api/tasks')); // all routes starting with /api/items should use the routes in this file
+
+// Serve static assets if in deployment
+if (process.env.NODE_ENV === 'production') {
+  app.use(express.static('client/build'));
+  app.get('*', (req, res) => {
+    res.sendFile(path.resolve(__dirname, 'client', 'build', 'index.html'));
+  });
+}
 
 const PORT = process.env.PORT || 8080;
 app.listen(PORT, () =>
