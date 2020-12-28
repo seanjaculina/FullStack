1d724f44bc579f3b3f224122c92db1a3aed0c859
Tanner Barcelos <tanner.manuel.barcelos@gmail.com>
2020-12-27 16:55:15 -0800

    cleanup

--- MERN/Traversy_Course/client/src/components/screens/Login.js
+++ MERN/Traversy_Course/client/src/components/screens/Login.js
@@ -1,10 +1,47 @@
 import React from 'react';
+import {
+  Col,
+  Row,
+  Button,
+  Form,
+  FormGroup,
+  Label,
+  Input,
+  Container,
+} from 'reactstrap';
 
 const Login = () => {
   return (
-    <div>
+    <Container>
       <h1>Login</h1>
-    </div>
+      <Form>
+        <Row form>
+          <Col md={6}>
+            <FormGroup>
+              <Label for="email">Email</Label>
+              <Input
+                type="email"
+                name="email"
+                id="email"
+                placeholder="Please enter an email"
+              />
+            </FormGroup>
+          </Col>
+          <Col md={6}>
+            <FormGroup>
+              <Label for="password">Password</Label>
+              <Input
+                type="password"
+                name="password"
+                id="password"
+                placeholder="Enter password"
+              />
+            </FormGroup>
+          </Col>
+        </Row>
+        <Button>Log in</Button>
+      </Form>
+    </Container>
   );
 };
 
--- MERN/Traversy_Course/client/src/components/screens/Register.js
+++ MERN/Traversy_Course/client/src/components/screens/Register.js
@@ -1,10 +1,64 @@
-import React from 'react';
+import React, { useState } from 'react';
+import {
+  Col,
+  Row,
+  Button,
+  Form,
+  FormGroup,
+  Label,
+  Input,
+  Container,
+} from 'reactstrap';
 
 const Register = () => {
+  const [email, setEmail] = useState('');
+  const [password, setPassword] = useState('');
+  const [name, setNmail] = useState('');
   return (
-    <div>
+    <Container>
       <h1>Register</h1>
-    </div>
+      <Form>
+        <Row form>
+          <Col md={6}>
+            <FormGroup>
+              <Label for="name">Email</Label>
+              <Input
+                type="text"
+                name="name"
+                value={name}
+                id="name"
+                placeholder="Please enter your name"
+              />
+            </FormGroup>
+          </Col>
+          <Col md={6}>
+            <FormGroup>
+              <Label for="email">Email</Label>
+              <Input
+                type="email"
+                name="email"
+                value={email}
+                id="email"
+                placeholder="Please enter an email"
+              />
+            </FormGroup>
+          </Col>
+          <Col md={6}>
+            <FormGroup>
+              <Label for="password">Password</Label>
+              <Input
+                type="password"
+                name="password"
+                value={password}
+                id="password"
+                placeholder="Enter password"
+              />
+            </FormGroup>
+          </Col>
+        </Row>
+        <Button>Register</Button>
+      </Form>
+    </Container>
   );
 };
 
