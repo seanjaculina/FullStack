b632a7fe4d01c5f40fe7a9a7fbfa25e67e46b0fe
Tanner Barcelos <tanner.manuel.barcelos@gmail.com>
2020-12-29 00:11:46 -0800

    updates

--- MERN/Traversy_Course/client/src/actions/authActions.js
+++ MERN/Traversy_Course/client/src/actions/authActions.js
@@ -1,9 +1,6 @@
 import axios from 'axios';
 import { returnErrors } from './errorActions';
 import {
-  USER_LOADED,
-  USER_LOADING,
-  AUTH_ERROR,
   LOGIN_SUCCESS,
   LOGIN_FAIL,
   LOGOUT_SUCCESS,
@@ -12,18 +9,6 @@ import {
   USER_UPDATE_SUCCESS,
   USER_UPDATE_FAIL,
 } from './types';
-
-// // Check token and load the user if it exists
-// export const loadUser = () => async (dispatch, getState) => {
-//   dispatch({ type: USER_LOADING });
-//   try {
-//     const user = await axios.get('/api/auth/user', tokenConfig(getState));
-//     dispatch({ type: USER_LOADED, payload: user.data });
-//   } catch (error) {
-//     dispatch(returnErrors(error.response.data, error.response.status));
-//     dispatch({ type: AUTH_ERROR });
-//   }
-// };
 
 // Register new user action
 export const register = ({ name, email, password }) => async (dispatch) => {
