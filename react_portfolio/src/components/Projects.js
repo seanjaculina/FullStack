b004d95464e8ea9120f2e705d28eef7e776661c1
Tanner Barcelos <tanner.manuel.barcelos@gmail.com>
2021-01-06 18:13:58 -0800

    project transition removal

--- react_portfolio/src/components/Projects.js
+++ react_portfolio/src/components/Projects.js
@@ -8,21 +8,19 @@ import ProjectItem from './ProjectItem';
 
 const Projects = () => {
   return (
-    <Fade>
-      <div className="section_container" id="projects">
-        <div className="projects">
-          <h1 className="section__heading">Projects</h1>
-          {ProjectList.map((project) => {
-            return (
-              <ProjectItem
-                data={project}
-                key={Math.floor(Math.random() * 10000)}
-              />
-            );
-          })}
-        </div>
+    <div className="section_container" id="projects">
+      <div className="projects">
+        <h1 className="section__heading">Projects</h1>
+        {ProjectList.map((project) => {
+          return (
+            <ProjectItem
+              data={project}
+              key={Math.floor(Math.random() * 10000)}
+            />
+          );
+        })}
       </div>
-    </Fade>
+    </div>
   );
 };
 
