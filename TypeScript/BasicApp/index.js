1196348cf87920076b0669bc70516127f1bcaec3
Tanner Barcelos <tanner.manuel.barcelos@gmail.com>
2021-04-01 12:41:11 -0700

    cleanup

--- TypeScript/BasicApp/index.js
+++
@@ -1,6 +0,0 @@
-"use strict";
-exports.__esModule = true;
-var axios_1 = require("axios");
-//const URL = 'https://jsonplaceholder.typicode.com/todos/1'; <-- fetches one todo
-var URL = 'https://jsonplaceholder.typicode.com/todos'; // <-- fetches ALL todos
-axios_1["default"].get(URL).then(function (res) { return console.log(res.data); });
