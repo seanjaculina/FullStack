7f9124ade060375f0685ff64fea8c2a23d9916f2
Tanner Barcelos <tanner.manuel.barcelos@gmail.com>
2021-04-05 01:11:45 -0700

    readme updates

--- data_engineering/web_scraping/cheerio/IMDb_scraper/scraper.js
+++ data_engineering/web_scraping/cheerio/IMDb_scraper/scraper.js
@@ -7,8 +7,9 @@ const baseURI = process.env.IMDB_URI;
 
 /**
  * Performs the request to the website to scrape based off the term user enters from the client
- * - Scrape the data, add it to the db, then return all documents in the database that match the query
- *
+ *  - Scrape the data  for all movies with the search term
+ *  - add all these new movie documents to an array to return as our JSON for this route
+ *  - Save all documents to the database (extract, translate and load done here [E.T.L])
  */
 const searchMovies = async (term) => {
   const page = await fetch(`${baseURI}${term}`);
@@ -19,19 +20,20 @@ const searchMovies = async (term) => {
   // Init cheerio to load the html being requested
   const $ = cheerio.load(html);
 
-  // Get every element in the table
+  // Get every element in the table and for every element, do some processing [notice the API is just like jQuery]
   $('.findResult').each(async (index, element) => {
     const el = $(element);
     const title = el.find('td.result_text a').text().trim(); // get the title of the element
     const image = el.find('td a img').attr('src'); // get the image source
     const moreInfoLink = el.find('td.result_text a').attr('href'); // get the link to more info on this movie
+    // create a new movie document for our mongoddb store
     const newMovie = new Movie({
       title,
       image_url: image,
       info_url: `https://imdb.com/${moreInfoLink}`,
     });
     movies.push(newMovie);
-    await newMovie.save();
+    await newMovie.save(); // commit it
   });
   return movies;
 };
