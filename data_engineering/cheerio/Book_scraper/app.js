ba20eade6f1c37f6384c1f713ddea9307387f317
Tanner Barcelos <tanner.manuel.barcelos@gmail.com>
2021-04-14 15:50:23 -0700

    refinement needed on logic

--- data_engineering/cheerio/Book_scraper/app.js
+++ data_engineering/cheerio/Book_scraper/app.js
@@ -2,32 +2,24 @@ const axios = require('axios');
 const cheerio = require('cheerio');
 const convertToUSD = require('./conversion');
 
-const URL = 'http://books.toscrape.com/';
+let URL = 'http://books.toscrape.com/';
 
 // Gets the HTML page - returns promise as it is an async func
-const getPage = async () => {
-  const { data } = await axios.get(URL);
+const getPage = async (url = URL) => {
+  const { data } = await axios.get(url);
   return data;
 };
 
-const scrapeAllProducts = async () => {
-  const html = await getPage();
-
-  // Init cheerio
-  const $ = cheerio.load(html);
-
+// Get number of pages of books to scrape
+const getNumberPages = async ($) => {
   // Get total results and the amount of results per page so we can loop all pages and get all data
   const totalResults = $('.form-horizontal strong:first-of-type').text();
   const totalOnPage = $('.form-horizontal strong:last-of-type').text();
   const totalPages = totalResults / totalOnPage;
+  return totalPages;
+};
 
-  const books = [];
-
-  for (let i = 1; i < totalPages + 1; i++) {
-    if (i === 1) {
-    }
-  }
-
+const parseHTML = ($, URL, books) => {
   $('div ol.row li').each((index, element) => {
     const book = $(element);
     const bookTitle = book.find('h3 a').attr('title');
@@ -44,6 +36,28 @@ const scrapeAllProducts = async () => {
     };
     books.push(bookDetails);
   });
+  console.log(books);
+};
+
+const scrapeAllProducts = async () => {
+  // Init cheerio
+  const html = await getPage();
+  const $ = cheerio.load(html);
+  const totalPages = getNumberPages($);
+
+  const books = [];
+
+  for (let i = 1; i < totalPages + 1; i++) {
+    // Index page - page 1
+    if (i === 1) {
+      parseHTML($, URL, books);
+    } else {
+      URL = `${URL}catalogue/page-${i}.html`;
+      const html = await getPage(URL);
+      const $ = cheerio.load(html);
+      parseHTML($, URL, books);
+    }
+  }
   return books;
 };
 
